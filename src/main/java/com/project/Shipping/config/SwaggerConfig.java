7baea5f62ac2acaf977bfd5cf4d219988ce0540d
mert <mk754@sussex.ac.uk>
2023-09-22 22:31:41 +0100

    Swagger updated with springdoc, Actuator-Prometheus-Grafana added

--- src/main/java/com/project/Shipping/config/SwaggerConfig.java
+++ src/main/java/com/project/Shipping/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.project.Shipping.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
