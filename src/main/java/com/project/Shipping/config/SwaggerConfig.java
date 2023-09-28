5c93b0e2db035f044f553981b36a88ab78eb7ebb
mert <mk754@sussex.ac.uk>
2023-09-28 23:35:09 +0100

    Redis Cache used in business flow, RabbitMQ integrated in docker, springdoc used instead of springdfox,

--- src/main/java/com/project/Shipping/config/SwaggerConfig.java
+++ src/main/java/com/project/Shipping/config/SwaggerConfig.java
@@ -7,32 +7,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.project.Fineros.controller"))
-                .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiInfoMetaData());
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Shipping")
+                        .version("1.0.0")
+                        .contact(new Contact().email("mertkaya2455@gmail.com"))
+                        .description("Shipping API"));
+
     }
-
-    private ApiInfo apiInfoMetaData() {
-
-        return new ApiInfoBuilder().title("Shipping")
-                .description("Shipping API")
-                .contact(new Contact("Mert Kaya", "https://www.linkedin.com/in/merttkayaa/", "mertkaya2455@gmail.com"))
-                .build();
-    }
-
 }
